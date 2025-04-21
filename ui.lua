fe7830316f110270d8a47e7c97c6e3f9d20e6936
Robloxexploiter691 <141586656+Robloxexploiter691@users.noreply.github.com>
2025-04-21 12:56:16 -0700

    Update ui.lua

--- ui.lua
+++ ui.lua
@@ -4,7 +4,7 @@ local username = player.Name
 
 game:GetService("StarterGui"):SetCore("SendNotification",{
 Title = "Blatant.cc",
-Text = "Thank you for executing Blatant.cc ".. username;
+Text = "Thank you for Buying Blatant.cc ".. username;
 Icon = "rbxassetid://114125211876798",
 Duration = 10,
 })
